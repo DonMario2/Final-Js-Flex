7b4ad6ae31995e162e5bf1b58a5655040239a678
Mario <mario.bravo.82@gmail.com>
2024-07-14 20:27:25 -0300

    basico html

--- app/app.js
+++ app/app.js
@@ -1,31 +1,24 @@
-const card = document.createElement('div');
-card.classList.add('card');
+window.addEventListener("load", crearTarjetas);
 
-const image = document.createElement('img');
-image.src = producto.imagen;
-image.classList.add('card-img-top');
-
-const cardBody = document.createElement('div');
-cardBody.classList.add('card-body');
-
-const title = document.createElement('h5');
-title.classList.add('card-title');
-title.textContent = producto.nombre;
-
-const description = document.createElement('p');
-description.classList.add('card-text');
-description.textContent = producto.descripcion;
-
-const price = document.createElement('p');
-price.classList.add('card-text');
-price.textContent = producto.precio;
-
-cardBody.appendChild(title);
-cardBody.appendChild(description);
-cardBody.appendChild(price);
-
-card.appendChild(image);
-card.appendChild(cardBody);
-
-// Agrega la tarjeta al contenedor de las tarjetas
-productosContainer.appendChild(card);
+function crearTarjetas() {
+  fetch('app/product.json')
+    .then(response => response.json())
+    .then(data => {
+      let output = '';
+      data.forEach(function(product) {
+        output += `
+          <div class="col-md-4">
+            <div class="card mb-4">
+              <img src="${product.imagen}" class="card-img-top" alt="${product.nombre}">
+              <div class="card-body">
+                <h5 class="card-title">${product.nombre}</h5>
+                <p class="card-text">${product.descripcion.join(', ')}</p>
+              </div>
+            </div>
+          </div>
+        `;
+      });
+      document.getElementById('product-cards').innerHTML = output;
+    })
+    .catch(err => console.log(err));
+}
